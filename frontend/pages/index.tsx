c7fbb71e68761e9c3418525497f915933849c2d1
mifranco-upenn <112339889+mifranco-upenn@users.noreply.github.com>
2023-04-28 14:47:27 -0400

    frontpage cleanup and basic grid styling

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,9 @@ import NavBar from '@/components/NavBar'
 import { useEffect, useState } from 'react'
 import { Stock } from '@/constants/types'
 
+import styles from '../components/homepage.module.css'
+import Link from 'next/link';
+
 export default function Home() {
 
   const [randomStock, setRandomStock] = useState<Stock | null>(null);
@@ -26,13 +29,20 @@ export default function Home() {
 
 
   return (
-  <div className="h-screen">
+  <div className={styles.container}>
     <NavBar />
-    <div>
-      <h1>Random Stock: {randomStock?.symbol}</h1>
+    <div className={styles.content}>
+      {/* <h1 className={styles.randomStock}>Random Stock: {randomStock?.symbol}</h1> */}
+      <h1 className={styles.randomStock}>
+        <Link href={`/stocks/${randomStock?.symbol}`}>Random Stock: {randomStock?.symbol} -- {randomStock?.security}</Link>
+      </h1>
+      <ul className={styles.hotStocks}>
       {hotStocks && hotStocks.map((s : Stock) => (
-        <li key={s.symbol}>{s.symbol} -- {s.security}</li>
+        <li key={s.symbol}>
+          <Link href={`/stocks/${s.symbol}`}>{s.symbol} -- {s.security}</Link>
+        </li>
       ))}
+      </ul>
     </div>
   </div> 
   )
