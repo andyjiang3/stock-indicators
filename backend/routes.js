4f9c878076397b3da3dc8f73b91a06cce9f2f446
Mohamed Abaker <mabaker@ist2-0751.apn.wlan.private.upenn.edu>
2023-04-29 01:23:31 -0400

    small fix

--- backend/routes.js
+++ backend/routes.js
@@ -701,7 +701,7 @@ const expRollingMean = (req, res) => {
           prevPosition = position
 
 
-          return { date: item.date, rolling_mean: ema, buy, sell, close:ema };
+          return { date: item.date, rolling_mean: ema, buy, sell, close:actual };
         });
         cache.set(req.url, results);
         res.send(results);
