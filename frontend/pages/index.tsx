e075e624185818d4578e83bc5e06b0528e7a309e
andy jiang <andyjiang55@yahoo.com>
2023-04-28 21:16:24 -0400

    home page done

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,7 +8,7 @@ import Link from "next/link"
 export default function Home() {
 
   const [randomStock, setRandomStock] = useState<Stock | null>(null);
-  const [hotStocks, setHotStocks] = useState<Stock[] | null>(null);
+  const [hotStocks, setHotStocks] = useState(null);
   const [topMovers, setTopMovers] = useState(null);
   const [mostVolatile, setMostVolatile] = useState(null);
 
@@ -65,14 +65,16 @@ export default function Home() {
       </div> 
       }
       <h1 className="text-xl mt-5 font-medium">Hot Stocks 🔥</h1>
-      <p className="mb-10">Determined using data analysis on news sentiment and market trends, stocks with most positive upticks are considered as hot stocks.</p>
+      <p className="mb-10">Stocks with the most postive upticks, determined using data analysis on news sentiment and market trends.</p>
       {hotStocks ? <div className="grid grid-cols-5 gap-4 h-46 mb-16">
-        {hotStocks.map((data, i) => {
+        {(hotStocks as any).map((data: any, i: any) => {
           return (<div className="relative hover:bg-blue-500 hover:text-white flex-row bg-white h-full w-full border border-gray-300 justify-center text-center p-1 pt-2 rounded-md">
               <Link href={`/stocks/${data.symbol}`}>
-                <span className="text-medium text-4xl absolute left-0 bottom-2/3 text-zinc-500">{i + 1}</span>
+                <span className="text-medium text-4xl absolute left-0 bottom-3/4 text-zinc-500">{i + 1}</span>
                 <div className="text-sm font-bold mb-3">{data.symbol}</div>
-                <div className="">{data.security}</div>
+                <div className="mb-3">{data.security}</div>
+                <div className="text-green-700">{Math.round(data.upticks * 100) / 100} positive upticks</div>
+
               </Link>
           </div>)
             
@@ -85,8 +87,8 @@ export default function Home() {
         }
      
       <h1 className="text-xl mt-5 font-medium">Top Movers 📈</h1>
-      <p className="mb-10">Stocks that had the biggest upward price movement from starting date to ending date</p>
-      {topMovers ? <div className="grid grid-cols-5 gap-4 h-46 mb-15">
+      <p className="mb-10">Stocks that had the biggest upward price movement from starting date to ending date.</p>
+      {topMovers ? <div className="grid grid-cols-5 gap-4 h-46 mb-16">
 
         {(topMovers as any).map((data: any, i: any) => {
           if (i < 10) {
@@ -96,6 +98,31 @@ export default function Home() {
                   <div className="text-sm font-bold mb-3">{data.symbol}</div>
                   <div className="mb-3">{data.security}</div>
                   <div className="text-green-700">${Math.round(data.price_change * 100) / 100} increase</div>
+                </Link>
+                
+            </div>)
+          }
+            
+        })
+        }
+        </div> : 
+        <div className="flex w-full justify-center align-center">
+          <CircularProgress/>
+        </div> 
+      }
+
+    <h1 className="text-xl mt-5 font-medium">Most Volatile 💥</h1>
+      <p className="mb-10">Stocks with the biggest price fluctuations from start date to end date.</p>
+      {mostVolatile ? <div className="grid grid-cols-5 gap-4 h-46 mb-15">
+
+        {(mostVolatile as any).map((data: any, i: any) => {
+          if (i < 10) {
+            return (<div className="relative hover:bg-blue-500 hover:text-white flex-row bg-white h-full w-full border border-gray-300 justify-center text-center p-1 pt-2 rounded-md">
+                <Link href={`/stocks/${data.symbol}`}>
+                  <span className="text-medium text-4xl absolute left-0 bottom-3/4 text-zinc-500">{i + 1}</span>
+                  <div className="text-sm font-bold mb-3">{data.symbol}</div>
+                  <div className="mb-3">{data.security}</div>
+                  <div className="text-red-700">{Math.round(data.volatility * 100) / 100} volatility</div>
                 </Link>
                 
             </div>)
