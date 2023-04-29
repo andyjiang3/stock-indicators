a854f9be16da132567b2fc9aeca2106d0f6b1f15
andy jiang <andyjiang55@yahoo.com>
2023-04-29 02:54:01 -0400

    fix trading strat page issue

--- frontend/pages/strategies/index.tsx
+++ frontend/pages/strategies/index.tsx
@@ -1,10 +1,25 @@
 import NavBar from "@/components/NavBar"
+import { Dispatch, SetStateAction } from "react"
 
-export default function TradingStrategies() {
+interface TradingStrategiesProps {
+    startDate: string,
+    setStartDate: Dispatch<SetStateAction<string>>,
+    endDate: string,
+    setEndDate: Dispatch<SetStateAction<string>>
+}
+
+const TradingStrategies = ({
+    startDate,
+    setStartDate,
+    endDate,
+    setEndDate
+}: TradingStrategiesProps) => {
     return (<>
-    <NavBar />
-    <div>List of Trading Strategies</div>
-    </>
+            <NavBar startDate={startDate} setStartDate={setStartDate} endDate={endDate} setEndDate={setEndDate}/>
+            <div>List of Trading Strategies</div>
+            </>
 
     )
 }
+
+export default TradingStrategies
